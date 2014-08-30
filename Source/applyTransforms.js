d411864c7ceb9c0a3ebe1ae8cc9c4913552f03b9
kentaromiura <kentaromiura@gmail.com>
2014-08-30 19:22:20 +0100

    reuse the single instance of transform3d

--- Source/applyTransforms.js
+++ Source/applyTransforms.js
@@ -1,5 +1,6 @@
 var transform3d = require('transform3d'),
-    transform = require('./transform')
+    transform = require('./transform'),
+    operations = require('transform3d/lib/operations');
 
 module.exports = function applyTransforms(){
   var properties = [
@@ -18,11 +19,17 @@ module.exports = function applyTransforms(){
       'scaleX',
       'scaleY',
       'scaleZ'],
-    transformation = new transform3d,
+    transformation,
     i,
     max,
     property;
 
+  if(!this._transform3d){
+     this._transform3d = new transform3d;
+     this._identity = this._transform3d.compose();
+  }
+  transformation = this._transform3d;
+  transformation.operations = [new operations.Matrix(this._identity)]
   for(i = 0, max = properties.length; i < max; i++){
     property = properties[i]
     if(this.hasAttribute(property))
