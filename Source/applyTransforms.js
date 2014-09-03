75c965e691879cc0b77d6bc69aa221c1b4fd1d79
Cristian Carlesso <kentaromiura@gmail.com>
2014-09-03 17:35:36 +0100

    revert the optimizations changes, as they're root of all the evil

--- Source/applyTransforms.js
+++ Source/applyTransforms.js
@@ -1,6 +1,5 @@
 var transform3d = require('transform3d'),
-    transform = require('./transform'),
-    operations = require('transform3d/lib/operations');
+    transform = require('./transform');
 
 module.exports = function applyTransforms(){
   var properties = [
@@ -19,17 +18,11 @@ module.exports = function applyTransforms(){
       'scaleX',
       'scaleY',
       'scaleZ'],
-    transformation,
-    i,
-    max,
-    property;
+      i,
+      max,
+      property,
+      transformation = new transform3d;
 
-  if(!this._transform3d){
-     this._transform3d = new transform3d;
-     this._identity = this._transform3d.compose();
-  }
-  transformation = this._transform3d;
-  transformation.operations = [new operations.Matrix(this._identity)]
   for(i = 0, max = properties.length; i < max; i++){
     property = properties[i]
     if(this.hasAttribute(property))
